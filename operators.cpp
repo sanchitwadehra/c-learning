a4cb9815f4185391e0567bcd4be8f9a57272d5c1
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-08-02 21:30:45 +0530

    Update operators.cpp

--- operators.cpp
+++ operators.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
     //Arithmetic Operators
     int i=10,j=20,k;
-    
+    //10    9      19    20    9     20    10    19
     k=i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
 
-    cout<<i<<endl;
-    cout<<j<<endl;
-    cout<<k<<endl;
+    cout<<i<<endl;//10
+    cout<<j<<endl;//20
+    cout<<k<<endl;//-20
 
     return 0;
 }
