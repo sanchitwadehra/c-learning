44909a8fb9a21ce2c69318ceb7b5bc4db6f0374f
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-08-02 13:04:26 +0530

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1;
+    float num2;
+    cout<<"Enter first number :-\n";
+    cin>>num1;
+    cout<<"Enter second number :-\n";
+    cin>>num2;
+    char op;
+    cout<<"Enter a operator :-\n";
+    cin>>op;
+    float sol;
+    cout<<"Solution :-\n";
+    if(op=='+'){
+        sol=num1+num2;
+        cout<<sol<<endl;
+    }
+    else if(op=='-'){
+        sol=num2-num1;
+        cout<<sol<<endl;
+    }
+    else if(op=='*'){
+        sol=num1*num2;
+        cout<<sol;
+    }
+    else if(op=='/'){
+        sol=num2/num1;
+        cout<<sol<<endl;
+    }
+    else {
+        cout<<"Sorry didn't recognise the operator\n";
+    }
+return 0;
+}
