eb3729c78669be8f86e0e1f250f7f5bdf20f3cef
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-08-23 22:07:49 +0530

    Update some_unique_beginner_questions.cpp

--- some_unique_beginner_questions.cpp
+++ some_unique_beginner_questions.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-
-int mian(){
-    
+int main(){
+    int num;
+    cout<<"Enter a number :- \n";
+    cin>>num;
+    bool flag=1;
+    for(int a=2;a<=sqrt(num);a++){
+        if(num%a==0){
+            cout<<"Non-Prime";
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1){
+        cout<<"Prime";
+    }
     return 0;
 }
