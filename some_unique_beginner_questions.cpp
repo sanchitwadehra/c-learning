731212a6286495a31a29d95bdd112fd3220c248e
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-08-23 22:12:15 +0530

    Update some_unique_beginner_questions.cpp

--- some_unique_beginner_questions.cpp
+++ some_unique_beginner_questions.cpp
@@ -6,10 +6,11 @@ int main(){
     cout<<"Enter a number :- \n";
     cin>>num;
     bool flag=1;
-    for(int a=2;a<=sqrt(num);a++){
+    for(int a=2;a<=sqrt(num);a++){//sqrt() is used here as if a number has factor until sqrt of it 
+    //then the factor after sqrt is actually a multiple of that first factor
         if(num%a==0){
             cout<<"Non-Prime";
-            flag=0;
+            flag=0;/*Unique way to know weather a If statement was executed or not*/
             break;
         }
     }
