06db1e9920b8ecac58b2801d3ec45f4e56c75297
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-09-08 22:20:36 +0530

    Update test_code_runner.cpp

--- test_code_runner.cpp
+++ test_code_runner.cpp
@@ -1,32 +1,28 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+
+int print(int num1,int num2){
+    int b;
+    for(int a=num1;a<=num2;a++){
+        for(int b=2;b<=a;b++){
+            if(a%b==0){
+                break;
+            }
+        }
+        if(b==a){
+            cout<<b<<endl;
+        }
+    }
+return 0;
+}
+
 int main(){
-    int num,number,lastdigit,cubed_int,sum,f1;
-    float cubed;
-    f1=0;
-    sum=0;
-    cout<<"Enter a number :-"<<endl;
-    cin>>num;
-    number=num;
-    while(number>=1){
-        lastdigit=(number%10);
-        cubed=pow(lastdigit,3);
-        cubed_int=static_cast<int>(cubed);
-        sum=cubed_int+sum;
-        number=number-lastdigit;
-        number=number/10;
-        f1++;
-    }
-    cout<<f1<<endl;
-    cout<<sum<<endl;
-    cout<<num<<endl;
-    cout<<number<<endl;
-    if(sum==num){
-        cout<<"Armstrong number"<<endl;
-    }
-    else{
-        cout<<"not a armstrong number"<<endl;
-    }
+    int num_1,num_2;
+    cout<<"Enter first number :-\n";
+    cin>>num_1;
+    cout<<"Enter second number :-\n";
+    cin>>num_2;
+    cout<<"The prime numbers between them is :-\n"<<print(num_1,num_2)<<endl;
     return 0;
 }
