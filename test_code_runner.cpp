49f75b2d53f34c45dbd598c5eb377d7989b15627
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-09-13 20:42:49 +0530

    Update test_code_runner.cpp

--- test_code_runner.cpp
+++ test_code_runner.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void print(int num1,int num2){
     int b;
+    cout<<"The prime numbers between them are :- "<<endl;
     for(int a=num1;a<=num2;a++){
         for(b=2;b<=a;b++){
             if(a%b==0){
@@ -17,18 +18,12 @@ void print(int num1,int num2){
 return;
 }
 
-int print_prime(int num_1,int num_2){
-    print(num_1,num_2);
-    return 0;
-}
-
 int main(){
     int a,b;
     cout<<"Enter first number :-\n";
     cin>>a;
     cout<<"Enter second number :-\n";
     cin>>b;
-    cout<<"The prime numbers between them is :- "<<endl;
-    print_prime(a,b);
+    print(a,b);
     return 0;
 }
