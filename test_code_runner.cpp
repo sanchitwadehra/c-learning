f4891d77a85a408c29309b22411c37ff45f24e9b
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-09-13 20:33:45 +0530

    Update test_code_runner.cpp

--- test_code_runner.cpp
+++ test_code_runner.cpp
@@ -17,13 +17,18 @@ void print(int num1,int num2){
 return;
 }
 
-int main(){
-    int num_1,num_2;
-    cout<<"Enter first number :-\n";
-    cin>>num_1;
-    cout<<"Enter second number :-\n";
-    cin>>num_2;
-    cout<<"The prime numbers between them is :- "<<endl;
+int print_prime(int num_1,int num_2){
     print(num_1,num_2);
+    return 0;
+}
+
+int main(){
+    int a,b;
+    cout<<"Enter first number :-\n";
+    cin>>a;
+    cout<<"Enter second number :-\n";
+    cin>>b;
+    cout<<"The prime numbers between them is :- "<<endl;
+    print_prime(a,b);
     return 0;
 }
