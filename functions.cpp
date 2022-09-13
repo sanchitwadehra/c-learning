6add2df2fd735cdcefbac6bcdf2a392e2c694552
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-09-13 20:45:24 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -6,6 +6,7 @@ using namespace std;
 //nothing written in return statement
 void print(int num1,int num2){
     int b;
+    cout<<"The prime numbers between them are :- "<<endl;
     for(int a=num1;a<=num2;a++){
         for(b=2;b<=a;b++){
             if(a%b==0){
@@ -19,21 +20,15 @@ void print(int num1,int num2){
 return;
 }
 
-int print_prime(int num_1,int num_2){
-    print(num_1,num_2);
-    return 0;
-}
-
 int main(){
     int a,b;
     cout<<"Enter first number :-\n";
     cin>>a;
     cout<<"Enter second number :-\n";
     cin>>b;
-    cout<<"The prime numbers between them is :- "<<endl;
     /*cout<<"The prime numbers between them is :- "<<print_prime(a,b)<<endl;
     This would have been wrong it is a function just like 
     if statement ,while statement ,etc. */
-    print_prime(a,b);
+    print(a,b);
     return 0;
 }
