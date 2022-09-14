d9969d736c2a22769e430eecbadf262acde461f3
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-09-14 18:48:09 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -20,6 +20,20 @@ void primes_between(int num1,int num2){
 return;
 }
 
+void fibonacci_between(int num1,int num2){
+    int a,b,c,d,f1;
+    f1=0;
+    b=1;
+    cout<<f1;
+    cout<<b;
+    for(a=0;f1<=num2;f1++){
+        a=b+a;
+        cout<<a;
+        b=a;
+    }
+    return;
+}
+
 int main(){
     int a,b;
     cout<<"Enter first number :-\n";
@@ -30,5 +44,6 @@ int main(){
     This would have been wrong it is a function just like 
     if statement ,while statement ,etc. */
     primes_between(a,b);
+    fibonacci_between(a,b);
     return 0;
 }
