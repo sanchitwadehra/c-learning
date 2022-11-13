4c550e63722e000a765c8a21c7cced84023bc27b
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-11-13 10:47:28 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,60 +1,30 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int n,c,j=1,k;
-    printf("Enter the size of array you want :- \n");
+    int n,x,k,a=1;
+    float final,sum,num,term;
+    printf("We are solving e^x = 1 + x +x^2/(2!) + x^3/(3!) ....");
+    printf("Enter the value of x :- \n");
+    scanf("%d",&x);
+    printf("How many terms of the given above formula do you want to calculate :- \n");
     scanf("%d",&n);
-    int a[n],z=1;
-    printf("Enter " "%d" " values into the Array :- \n",n);
-    for(int i=1;i<=(n);i++){
-        /*printf("Testing loop count " "%d""\n",j);
-        j++;*/
-        scanf("%d",&a[(i-1)]);
-    }
-    
-    //To print the array
-    printf("Values of the Array entered are as follows :- \n");
-    for(int i=1;i<=(n);i++){
-        printf("%d \n",a[(i-1)]);
-    }
-    
-    //To find the smallest value in Array :-
-    for(int i=1;i<=(n);i++){
-        (a[z]>=a[(i-1)])?(z=(i-1)):(z=z);
-    }
-    printf("The smallest value in array is :- ""%d""\n",a[z]);
-
-
-    //To sort in ascending order :-
-    for(int i=1;i<=(n);i++){
-        for(int j=i;j<=(n-1);j++){
-            if(a[(i-1)]<=a[j]){
-                a[(i-1)]=a[(i-1)];
-            }
-            else{
-                c=a[j];
-            a[j]=a[(i-1)];
-            a[(i-1)]=c;
-            }
+    printf("\n\n");
+    k=(n-1);
+    sum=0;
+    for(int i=1;i<=k;i++){
+        num=pow(x,i);
+        for(int j=1;j<=i;j++){
+            a=a*j;
+            printf("%d \n",a);
         }
+        printf("%d \n",a);
+        printf("%d \n",num);
+        term=(float)(num/a);
+        sum=sum+term;
     }
-
-    //To print the array
-    printf("array arranged in ascending order :- \n");
-    for(int i=1;i<=(n);i++){
-        printf("%d \n",a[(i-1)]);
-    }
-
-    //To find duplicates in the array :-
-    for(int i=1;i<=n;i++){
-        c=0;
-        k=0;
-        //Counting mechanism of repeated items in array
-        for(int j=1;j<=n;j++){
-            if(a[i-1]==a[j-1]){
-                k++;
-            }
-        }
-        //printf("%d ""is repeated %d times \n",a[i-1],k);
-    }
+    final=sum+1;
+    printf("So e^%d = 1 + %d +%d^2/(2!) + %d^3/(3!) .... \n",x,x,x,x);
+    printf("Calculated till n terms is :- \n");
+    printf("%f",final);
     return 0;
 }
