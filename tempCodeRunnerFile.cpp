42d10af6a2d89eecacb57ced9920edb2e55f54e4
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-11-14 17:57:22 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     int ul,ll,ip,f,op,nd,temp,sum,copy,extra;
     printf("To find Armstrong numbers between 2 numbers \n");
@@ -35,27 +36,7 @@ int main(){
         printf("copy out loop %d \n",copy);
         if(sum==copy){
             printf("%d \n",sum);
-    int n,x,k,num,a;
-    float final,sum,term;
-    printf("We are solving e^x = 1 + x +x^2/(2!) + x^3/(3!) ....");
-    printf("Enter the value of x :- \n");
-    scanf("%d",&x);
-    printf("How many terms of the given above formula do you want to calculate :- \n");
-    scanf("%d",&n);
-    k=(n-1);
-    sum=0;
-    for(int i=1;i<=k;i++){
-        a=1;
-        num=pow(x,i);
-        for(int j=1;j<=i;j++){
-            a=a*((float)j);
         }
-        term=(float)(((float)num)/((float)a));
-        sum=sum+term;
     }
-    final=sum+1;
-    printf("So e^%d = 1 + %d +%d^2/(2!) + %d^3/(3!) .... \n",x,x,x,x);
-    printf("Calculated till %d terms is :- \n",n);
-    printf("%f",final);
     return 0;
 }
