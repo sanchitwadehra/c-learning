377b4df5c8769c85c37814f62750d8c6e8c1eb39
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-11-22 21:15:01 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,20 +1,4 @@
 #include  <stdio.h>
-int add(int i, int j)
-{
-    return (i + j);
-}
-int subtract(int i, int j)
-{
-    return (i - j);
-}
-int division(int i, int j)
-{
-    return (i / j);
-}
-int multiply(int i, int j)
-{
-    return (i * j);
-}
 int main()
 {
     int n, ip;
