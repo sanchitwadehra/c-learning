9d04996c71ceb1b17fc32ec6558da5926e81c048
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-11-30 21:23:03 +0530

    corrections

--- functions.cpp
+++ functions.cpp
@@ -158,17 +158,29 @@ int add(int x,int y){
 
 
 Variables management in functions :-
+//IMPORTANT :- RUN THIS PROGRAM AND YOU WILL UNDERSTAND
 #include  <stdio.h>
-void swap(int *,int *);
+void swap_cv(int,int); //call by value 
+void swap_cr(int *,int *); //call by reference
 int main(){
     int a,b;
     scanf("%d%d",&a,&b);
     printf("Value of a is %d and value of b is %d \n",a,b);
-    swap(&a,&b);
+    swap_cv(a,b);
+    printf("After using call by value function : -\n");
+    printf("Value of a is %d and value of b is %d \n",a,b);
+    swap_cr(&a,&b);
+    printf("After using call by reference function : -\n");
     printf("Value of a is %d and value of b is %d \n",a,b);
     return 0;
 }
-void swap(int *a,int *b){
+void swap_cv(int a,int b){
+    int c;
+    c=a;
+    a=b;
+    b=c;
+}
+void swap_cr(int *a,int *b){
     int c;
     c=*a;
     *a=*b;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,26 +1,27 @@
-#include<stdio.h>
+#include  <stdio.h>
+void swap_cv(int,int);
+void swap_cr(int *,int *);
 int main(){
-    /*for(int i=1;i<=5;i++){
-        for(int j=5;j>=i;j--){
-            printf("%d ",j);
-        }
-        printf("\n");
-    }*/
-    int year;
-    printf("Enter a year :- \n");
-    scanf("%d",&year);
-    
-    if(year%400 == 0){
-        printf("%d is a leap year\n",year);
-    }
-    else if(year%100 == 0){
-        printf("%d isn't a leap year\n",year);
-    }
-    else if(year%4 == 0){
-        printf("%d is a leap year\n",year);
-    }
-    else{
-        printf("%d isn't a leap year\n",year);
-    }
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("Value of a is %d and value of b is %d \n",a,b);
+    swap_cv(a,b);
+    printf("After using call by value function : -\n");
+    printf("Value of a is %d and value of b is %d \n",a,b);
+    swap_cr(&a,&b);
+    printf("After using call by reference function : -\n");
+    printf("Value of a is %d and value of b is %d \n",a,b);
     return 0;
+}
+void swap_cv(int a,int b){
+    int c;
+    c=a;
+    a=b;
+    b=c;
+}
+void swap_cr(int *a,int *b){
+    int c;
+    c=*a;
+    *a=*b;
+    *b=c;
 }
