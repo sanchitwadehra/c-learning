594d252461e285f8c95d124ad4547d11ef33d2a1
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-07 21:23:00 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -209,3 +209,56 @@ int fact(int num){
     }
 }
 */
+
+/*
+#include<stdio.h>
+int loop(int,int);
+int main(){
+    printf("Enter a number whose table you want :- \n");
+    int a;
+    scanf("%d",&a);
+    printf("Enter the number till where you want it :- \n");
+    int b;
+    scanf("%d",&b);
+    loop(a,b);
+    return 0;
+}
+//This will run
+int loop(int a,int b){
+    if(b==0){
+        return 0;
+    }
+        return loop(a,b-1);  
+    printf("%d X %d = %d \n",a,b,a*b);
+}
+/*
+This will run
+    
+         loop(a,b-1);  
+printf("%d X %d = %d \n",a,b,a*b);
+    
+    */
+/*
+This will run
+int loop(int a,int b){
+    if(b==0){
+        return 0;
+    }
+    else{
+        return loop(a,b-1);  
+        printf("%d X %d = %d \n",a,b,a*b);
+    }
+}
+*/
+/*
+This will not run
+int loop(int a,int b){
+    if(b==0){
+        return 0;
+    }
+    else{
+        return loop(a,b-1); 
+    } 
+    printf("%d X %d = %d \n",a,b,a*b);
+}
+*/
