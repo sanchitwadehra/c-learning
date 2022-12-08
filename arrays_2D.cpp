5682a0239f450639420092a2647d70a8b8e25498
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-08 14:24:19 +0530

    interchanging diagonals in an array

--- arrays_2D.cpp
+++ arrays_2D.cpp
@@ -194,3 +194,48 @@ int main()
     }
     return 0;
 }
+
+//For interchanging diagonals of array
+#include <stdio.h>
+int main()
+{
+    int a, b, c, i, j;
+    printf("Enter rows of array :- \n");
+    scanf("%d", &a);
+    printf("Enter coloumns of array :- \n");
+    scanf("%d", &b);
+    int  arr[a][b];
+    printf("Enter the values into array :- \n");
+    for (i = 0; i < a; i++)
+    {
+        for (j = 0; j < b; j++)
+        {
+            scanf("%d", & arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("array that you entered is :- \n");
+    for (i = 0; i < a; i++)
+    {
+        for (j = 0; j < b; j++)
+        {
+            printf("%d ",  arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i=0;i<a;i++){
+        c=arr[i][b-i-1];
+        arr[i][b-i-1]=arr[i][i];
+        arr[i][i]=c;
+    }
+    printf("After Changing the Diagonals the array is :- \n");
+    for (i = 0; i < a; i++)
+    {
+        for (j = 0; j < b; j++)
+        {
+            printf("%d ",  arr[i][j]);
+        }
+        printf("\n");
+    }
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,67 +1,43 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 int main()
 {
-    int a,b;
-    char s1[100],s2[100];
-    printf("Enter a string.\n");
-    scanf("%s",s1);
-    //strlen(),strrev(),strcat(),strcpy(),strupr(),strlwr(),strcmp()
-    printf("Enter the operation you wish to perform on the string:\n1. String Length\n2. String Reverse\n3. String Concatenation\n4. String Copy\n5. String Upper Case\n6. String Lower Case\n7. String Comparison\n");
-    scanf("%d",&a);
- 
-    switch(a)
+    int a, b, c, i, j;
+    printf("Enter rows of array :- \n");
+    scanf("%d", &a);
+    printf("Enter coloumns of array :- \n");
+    scanf("%d", &b);
+    int  arr[a][b];
+    printf("Enter the values into array :- \n");
+    for (i = 0; i < a; i++)
     {
-    case 1:
+        for (j = 0; j < b; j++)
         {
-            b=strlen(s1);
-            printf("String Length = %d",b);
-            break;
+            scanf("%d", & arr[i][j]);
         }
-    case 2:
-        {
-            printf("String Reverse = %s",strrev(s1));
-            break;
-        }
-    case 3:
-        {
-            printf("Enter the target string.\n");
-            scanf("%s",s2);
-            strcat(s2,s1);
-            printf("Result - %s",s2);
-            break;
-        }
-    case 4:
-        {
-            strcpy(s2,s1);
-            printf("Copied String - %s",s2);
-            break;
-        }
-    case 5:
-        {
-            printf("The Upper Case of String is %s",strupr(s1));
-            break;
-        }
-    case 6:
-        {
-            printf("The Lower Case of String is %s",strlwr(s1));
-            break;
-        }
-    case 7:
-        {
-            printf("Enter the string you wish to compare with the previous string.\n");
-            scanf("%s",s2);
-            a=strcmp(s1,s2);
-            //returns -1 for not same , 0 for exactly same
-            //and 1 for same but difference of upper case lower case
-            printf("String Compare %d",a);
-            break;
-        }
- 
-    default:
-        {
-            printf("You have not entered a valid option.");
-        }
+        printf("\n");
     }
-return 0;
+    printf("array that you entered is :- \n");
+    for (i = 0; i < a; i++)
+    {
+        for (j = 0; j < b; j++)
+        {
+            printf("%d ",  arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i=0;i<a;i++){
+        c=arr[i][b-i-1];
+        arr[i][b-i-1]=arr[i][i];
+        arr[i][i]=c;
+    }
+    printf("After Changing the Diagonals the array is :- \n");
+    for (i = 0; i < a; i++)
+    {
+        for (j = 0; j < b; j++)
+        {
+            printf("%d ",  arr[i][j]);
+        }
+        printf("\n");
+    }
 }
