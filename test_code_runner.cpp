93993be19399b33d53ef18446efa0c0a468844c8
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-08 13:59:45 +0530

    update string

--- test_code_runner.cpp
+++ test_code_runner.cpp
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <string.h>
-int sglen(char s1[]){
-    int c=0;
-    for(int i=0;s1[i]!='\0';i++){
+int sglen(char s1[])
+{
+    int c = 0;
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
         c++;
     }
     return c;
 }
-char sgrev(char s1[]){
-    char s2[sglen(s1)];
-    for(int i=0;s1[i]!='\0';i++){
-        s2[(sglen(s1))-1-i]=s1[i];
+void sgrev(char s1[])
+{
+    char s2[100];
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
+        s2[(sglen(s1)) - 1 - i] = s1[i];
     }
     printf("String Reverse = ");
-    puts(s2);
-    return 0;
+
+    fputs(s2, stdin);
 }
+void sgcat(char s2[], char s1[])
+{
+    int i,j;
+    char s3[(sglen(s1)) + (sglen(s2))+1];
+    for ( i = 0; s1[i] != '\0'; i++)
+    {
+        s3[i] = s1[i];
+    }
+    for (j = 0; s2[j] != '\0'; j++)
+    {
+        s3[(sglen(s1)) + j] = s2[j];
+    }
+    s3[i+j]='\0';
+    puts(s3);
+    // fputs(s3,stdin);
+}   
 int main()
 {
     int a, b;
-    char s1[100], s2[100];
+    char s1[100];
+    char s2[100];
     printf("Enter a string.\n");
-    scanf("%s", s1);
-    // strlen(),strrev(),strcat(),strcpy(),strupr(),strlwr(),strcmp()
+    gets(s1);
+    // scanf("%s",s1);
+    //  strlen(),strrev(),strcat(),strcpy(),strupr(),strlwr(),strcmp()
     printf("Enter the operation you wish to perform on the string:\n1. String Length\n2. String Reverse\n3. String Concatenation\n4. String Copy\n5. String Upper Case\n6. String Lower Case\n7. String Comparison\n");
     scanf("%d", &a);
     switch (a)
@@ -40,10 +62,14 @@ int main()
     }
     case 3:
     {
+        char c;
         printf("Enter the target string.\n");
-        scanf("%s", s2);
-        strcat(s2, s1);
-        printf("Result - %s", s2);
+        scanf("%c", &c);
+        gets(s2);
+        puts(s2);
+        // fgets(s2,sizeof(s2),stdin);
+        // scanf("%s",s2);
+        sgcat(s2, s1);
         break;
     }
     case 4:
