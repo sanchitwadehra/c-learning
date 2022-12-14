ff8782f5922ed42d724ed5070c8a0888042fc073
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-14 12:03:08 +0530

    clarity to pointers concept added

--- pointer.cpp
+++ pointer.cpp
@@ -3,11 +3,27 @@
 int main(){
   int a;
   a=5;
+  int b[5]={1,215,15,2,8};
   int *p;
+  int *q;
   //2 bytes for pointers is 16bit Processor
   //4 bytes for poimters in 32bit Processor
   //8 bytes for pointers in 64bit Processor
   p=&a;
+  p=b;
+  //No need of p=&b[0];
+  //it is be default taking the first address
+  //without "&" but just in case of array and strings
+  /*Error given :-
+  q=a;
+  printf("%d \n",q);
+  */
+  /*
+  char *s;
+  s=&a;
+  you can't assign char pointer to a int address it will give runtime 
+  printf("%d \n",s);
+  */
   printf("%d \n",p);
   printf("%d \n",a);
   printf("%d \n",&a);
