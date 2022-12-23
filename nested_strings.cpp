4d4c2a69219ebb4e951424adf19567fbea4f4058
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-23 06:14:05 +0530

    Create nested_strings.cpp

---
+++ nested_strings.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+void jugaad(int n,int s1[],int s2[]){
+    struct customer{
+    char name[20];
+    char id[15];
+    int amount;
+    float qty;
+    struct address s2[40];
+};
+struct address{
+    char hno[15];
+    char city[15];
+    char state[15];
+    int pincode;
+};
+}
+int main(){
+    int n;
+    printf("Enter the amount enteries you want to make :- \n");
+    scanf("%d",&n);
+    int s1[n];
+    int s2[n];
+    jugaad()
+    struct customer s1[n];
+    struct address s2[n];
+
+    return 0;
+}
