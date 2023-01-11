f03a146c289aa9f42bf5d473946a9b09f9fd9f5a
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-01-11 13:40:11 +0530

    nested structures not completed

--- nested_structures.cpp
+++ nested_structures.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-void jugaad(int n,int s1[],int s2[]){
+//void jugaad(int n,int s1[],int s2[]){
     struct customer{
     char name[20];
     char id[15];
@@ -14,14 +14,14 @@ struct address{
     char state[15];
     int pincode;
 };
-}
+//}
 int main(){
     int n;
     printf("Enter the amount enteries you want to make :- \n");
     scanf("%d",&n);
     int s1[n];
     int s2[n];
-    jugaad()
+    //jugaad();
     struct customer s1[n];
     struct address s2[n];
 
