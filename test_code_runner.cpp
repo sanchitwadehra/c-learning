af9c4b19d8707bdaf0b9cd03e64a0c3105fd7a7a
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-01-24 19:28:10 +0530

    temporary

--- test_code_runner.cpp
+++ test_code_runner.cpp
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main(){
     int a=32;
+    int *p;
+    p=&a;
     printf("%d \n",a);
     printf("%d \n",&a);
     printf("%d \n",&a+1);
     printf("%x \n",&a+1);
     printf("%p \n",&a);
+
+    printf("%x \n",p);
     return 0;
 }
