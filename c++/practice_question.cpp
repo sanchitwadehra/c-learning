7711623207e6945090ffef8b9d525cd14470a33f
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-03-03 10:07:28 +0530

    Create practice_question.cpp

---
+++ c++/practice_question.cpp
@@ -0,0 +1,12 @@
+/*
+Write a Program to create a database of the following items Name(string),Roll No.(int),Height(cm),Weight(kg) create a constructor to intiialise the value create display function to display detail illustration of the use of copy constructor also use the concept of destructor 
+*/
+#include<iostream>
+using namespace std;
+class db{
+
+};
+int main(){
+
+    return 0;
+}
