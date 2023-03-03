2697fcbed4349c163673bea48ff44019f5c59185
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-03-03 10:13:20 +0530

    practice question in class

--- c++/practice_question.cpp
+++ c++/practice_question.cpp
@@ -1,12 +1,74 @@
 /*
-Write a Program to create a database of the following items Name(string),Roll No.(int),Height(cm),Weight(kg) create a constructor to intiialise the value create display function to display detail illustration of the use of copy constructor also use the concept of destructor 
+Write a Program to create a database of the following items Name(string),Roll No.(int),Height(cm),Weight(kg) create a constructor to intiialise the value create display function to display detail illustration of the use of copy constructor also use the concept of destructor
 */
+/*
 #include<iostream>
 using namespace std;
 class db{
-
+    string name;
+    int roll;
+    float height;
+    float weight;
 };
 int main(){
 
     return 0;
+}
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int rollNo;
+    float height;
+    float weight;
+
+    // Constructor to initialize values
+    Student(string n, int r, float h, float w)
+    {
+        name = n;
+        rollNo = r;
+        height = h;
+        weight = w;
+    }
+
+    // Copy constructor
+    Student(const Student &s)
+    {
+        name = s.name;
+        rollNo = s.rollNo;
+        height = s.height;
+        weight = s.weight;
+    }
+
+    // Destructor
+    ~Student()
+    {
+        cout << "Destructor called for " << name << endl;
+    }
+
+    // Display function to display details
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll No.: " << rollNo << endl;
+        cout << "Height: " << height << " cm" << endl;
+        cout << "Weight: " << weight << " kg" << endl;
+    }
+};
+
+int main()
+{
+    Student s1("Sanchit", 1, 180, 70.0f);
+    s1.display();
+
+    Student s2(s1); // Using copy constructor
+    s2.display();
+
+    return 0; // Destructor will be called for both objects here
 }
