59db2095c557a49cfa0882d3a4c100fc38687f1a
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-05-04 20:24:14 +0530

    Create file_handling.cpp

---
+++ c++/file_handling.cpp
@@ -0,0 +1,51 @@
+//WAP to take in multi word string 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int Max=80;
+    char str[Max];
+    cout<<"Enter a multi word string :- "<<endl;
+    cin.get(str,Max);
+    cout<<"String entered is :- "<<str<<endl;
+    return 0;
+    
+}
+*/
+//WAP to read multiple lines using a get function
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int Max=80;
+    char str[Max];
+    cout<<"Enter a multi word string :- "<<endl;
+    cin.get(str,Max,'#');
+    cout<<"String entered is :- "<<str<<endl;
+    return 0;
+}
+*/
+//WAP to demostrate use of write function
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int Max=80;
+    char str[Max];
+    cout<<"Enter a multi word string :- "<<endl;
+    cin.get(str,Max);
+    cout.write(str,5);
+    return 0;
+}
+*/
+//WAP to create a file stud.txt and write using << operator
+#include<iostream>
+using namespace std;
+int main(){
+    int uid;
+    string name;
+    return 0;
+}
+//WAP to read the content of a file using >> operator
+//WAP to read multiple lines from the file stud.txt
+
