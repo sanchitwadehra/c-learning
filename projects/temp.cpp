1d4460c387c9957ea78cb6d1fe634001a7d65aac
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-19 21:52:39 +0530

    Update temp.cpp

--- projects/temp.cpp
+++ projects/temp.cpp
@@ -3,21 +3,6 @@ using namespace std;
 
 int main()
 {
-   cout << "Running" << endl;
-   char abc;
-   cin >> abc;
-#ifdef _WIN32
-   if (abc == 's')
-   {
-      system("cls"); // Clears the screen on Windows
-      cout << "new screen" << endl;
-   }
-#else
-   if (abc == 's')
-   {
-      system("clear"); // Clears the screen on Unix-based systems
-      cout << "new screen" << endl;
-   }
-#endif
+   
    return 0;
 }
