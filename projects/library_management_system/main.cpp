9e3c07bba256b303e2f168f72ea2f01e64fa3065
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-21 06:14:07 +0530

    working on a simple draft

--- projects/library_management_system/main.cpp
+++ projects/library_management_system/main.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
 using namespace std;
 
+class human{
+    string name;
+    int age;
+    char gender;
+};
+
+class book{
+    string name;
+    int page_length;
+    int published_year;
+    int number_of_copies;
+};
+
+class user: public human{
+    
+};
+
+class staff{
+
+};
+
+class author{
+
+};
+
 int main(){
     
     return 0;
--- projects/temp.cpp
+++ projects/temp.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-   
-   return 0;
+class BankAccount {
+public:
+    string accountHolderName;
+    string accountNumber;
+    double balance;
+};
+
+int main() {
+    BankAccount userAccount;
+    userAccount.accountHolderName = "John Doe";
+    userAccount.accountNumber = "123456789";
+    userAccount.balance = 1000;
+
+    cout << "Account Holder: " << userAccount.accountHolderName << endl;
+    cout << "Account Number: " << userAccount.accountNumber << endl;
+    cout << "New Balance: " << userAccount.balance << endl;
+
+    return 0;
 }
