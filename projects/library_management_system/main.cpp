9adcadb53f7ba14fd31c457414d8288b8f3d64a5
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-22 07:04:53 +0530

    Update main.cpp

--- projects/library_management_system/main.cpp
+++ projects/library_management_system/main.cpp
@@ -1,29 +1,44 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 class human{
     string name;
     int age;
     char gender;
+    int phone_number[10];
 };
 
 class book{
     string name;
+    vector<int> author_IDs;
     int page_length;
     int published_year;
-    int number_of_copies;
+    int total_number_of_copies;
+    int number_of_issued_copies;
+    vector<string> tags;
 };
 
-class user: public human{
-    
+class issued{
+    int issuing_ID;
+    int book_ID;
+    int number_of_copies_issued;
+};
+
+class student: public human{
+    int student_ID;
+    vector<int> issuing_IDs;
 };
 
 class staff: public human{
-
+    int staff_ID;
 };
 
 class author: public human{
-
+    int author_ID;
+    vector<int> book_IDs;
+    vector<pair<string,int>> tags_by_frequency;
 };
 
 int main(){
