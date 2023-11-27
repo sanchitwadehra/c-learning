e76bc3dffae585c422f59080de20e2a0871861bc
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-28 00:20:06 +0530

    Update student.cpp

--- projects/library_management_system/student.cpp
+++ projects/library_management_system/student.cpp
@@ -1,4 +1,15 @@
 #include "sample_data.cpp"
-void student(){
-    
+void student()
+{
+    char i;
+    cout << "Are you looking for a specific book? (y/n)\n";
+    cin >> i;
+    if (i == 'Y' || i == 'y')
+    {
+        
+    }
+    else
+    {
+        cout << "Explore the Genres :-\n";
+    }
 }
