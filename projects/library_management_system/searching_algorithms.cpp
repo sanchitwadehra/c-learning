61ecab4f44637f159934722245756438a6e16a37
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-12-30 12:35:50 +0530

    Update searching_algorithms.cpp

--- projects/library_management_system/searching_algorithms.cpp
+++ projects/library_management_system/searching_algorithms.cpp
@@ -1,37 +1,32 @@
 #include "sample_data.cpp"
-int calculateDifference(const std::string& s1, const std::string& s2) {
-    int len1 = s1.length();
-    int len2 = s2.length();
+Book closestObject(vector<Book>& arr, const string& target) {
+    int closestIndex = -1;
+    int minDifference = numeric_limits<int>::max();
 
-    std::vector<std::vector<int>> dp(len1 + 1, std::vector<int>(len2 + 1, 0));
-
-    for (int i = 0; i <= len1; ++i) {
-        for (int j = 0; j <= len2; ++j) {
-            if (i == 0) {
-                dp[i][j] = j;
-            } else if (j == 0) {
-                dp[i][j] = i;
-            } else if (s1[i - 1] == s2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = 1 + std::min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
-            }
-        }
+    // Check for double quotes in input and handle accordingly
+    string searchTerm = target;
+    size_t found = searchTerm.find("\"");
+    if (found != string::npos) {
+        searchTerm.erase(remove(searchTerm.begin(), searchTerm.end(), '\"'), searchTerm.end());
     }
 
-    return dp[len1][len2];
-}
-
-Book closestObject(std::vector<Book>& arr, const std::string& target) {
-    int closestIndex = -1;
-    int minDifference = std::numeric_limits<int>::max();
+    // Convert target to lowercase and remove spaces
+    transform(searchTerm.begin(), searchTerm.end(), searchTerm.begin(), ::tolower);
+    searchTerm.erase(remove_if(searchTerm.begin(), searchTerm.end(), ::isspace), searchTerm.end());
 
     for (size_t i = 0; i < arr.size(); ++i) {
-        int difference = calculateDifference(arr[i].name, target);
+        // Convert bookName to lowercase and remove spaces
+        string bookName = arr[i].name;
+        transform(bookName.begin(), bookName.end(), bookName.begin(), ::tolower);
+        bookName.erase(remove_if(bookName.begin(), bookName.end(), ::isspace), bookName.end());
 
-        if (difference < minDifference) {
-            minDifference = difference;
-            closestIndex = i;
+        size_t found = bookName.find(searchTerm);
+        if (found != string::npos) {
+            int difference = abs(static_cast<int>(bookName.length() - searchTerm.length()));
+            if (difference < minDifference) {
+                minDifference = difference;
+                closestIndex = i;
+            }
         }
     }
 
